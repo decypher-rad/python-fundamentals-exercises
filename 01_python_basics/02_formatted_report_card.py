d552b0c38fbc95a09b5bb40831c72df1bdcf93b1
Rad <anooprad5@gmail.com>
2026-02-17 23:17:00 +0000

    Add Task 02 - formatted report card

---
+++ 01_python_basics/02_formatted_report_card.py
@@ -0,0 +1,17 @@
+"""
+Topic 2 - formatted report card (medium)
+Print a formatted report card like below:
+---------------------------
+Student name   : George
+Course         : Python
+Batch          : 2018
+---------------------------
+"""
+
+name = "George"
+course = "Python"
+batch = "2018"
+
+print("----------------------")
+print("Student name : " + name + "\n" + "Course       : " + course + "\n" + "Batch        : " + batch) #f string is used in the later exercises
+print("----------------------")
