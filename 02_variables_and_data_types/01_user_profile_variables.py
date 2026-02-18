bbf24bb8336d38ddbdc05b0d22117a84cf415bd4
Rad <anooprad5@gmail.com>
2026-02-18 23:21:39 +0000

    Create 01_user_profile_variables.py

---
+++ 02_variables_and_data_types/01_user_profile_variables.py
@@ -0,0 +1,18 @@
+"""
+Task 1: User profile variables
+Create the following variables:
+- name (str)
+- age (int)
+- height (float)
+- is_married (bool)
+"""
+
+name = "George"
+age = 22
+height = 186
+is_married = False
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_married))
