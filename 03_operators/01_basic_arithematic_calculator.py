61231ed0facbf2d048a26b749db41c86831d0e93
Rad <anooprad5@gmail.com>
2026-02-20 19:59:24 +0000

    Create 01_basic_arithematic_calculator.py

---
+++ 03_operators/01_basic_arithematic_calculator.py
@@ -0,0 +1,14 @@
+"""
+Task 1 : Basic Arithemetic Calculator (easy)
+Take two integers a and b, print:
+- sum
+- difference
+- product
+"""
+
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
